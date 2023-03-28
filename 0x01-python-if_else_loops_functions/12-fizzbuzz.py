acfe73b37fd4d2d9c8c52b8e94c71d04cd2829f8
Njau Steven <Njau.steven@gmail.com>
2023-03-28 22:57:52 +0300

    Print fizz buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,7 +5,7 @@ def fizzbuzz():
             print("Fizz", end=" ")
         elif n % 5 == 0:
             print("Buzz", end=" ")
-        elif n % 15 == 0:
+        elif n % 3 == 0 and n % 5 == 0:
             print("Fizzbuzz", end=" ")
         else:
             print("{}".format(n), end=" ")
